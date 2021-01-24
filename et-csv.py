3b2dd7b8f72cf802c747546ebf51131da26ce2ae
Xan <xanfpq@gmail.com>
2021-01-24 14:29:59 +0100

    Finish main process, pending excepts

--- et-csv.py
+++ et-csv.py
@@ -6,6 +6,7 @@ import shutil
 import chardet
 import tempfile
 import openpyxl
+import logging
 
 
 def get_header_check(header, delimiter, header_valid):
@@ -44,12 +45,12 @@ def get_header_check(header, delimiter, header_valid):
 
 def get_details_file(file_path, header=None):
     if header is None:
-        details = {'encoding': '', 'confidence': 0.0, 'delimiter': '', 'rows': 0, 'header': ''}
+        details = {'extension': '', 'encoding': '', 'confidence': 0.0, 'delimiter': '', 'rows': 0, 'header': ''}
     else:
-        details = {'encoding': '', 'confidence': 0.0, 'delimiter': '', 'rows': 0, 'header': '', 'check_header': ''}
+        details = {'extension': '', 'encoding': '', 'confidence': 0.0, 'delimiter': '', 'rows': 0, 'header': '', 'check_header': ''}
     if os.path.isfile(file_path):
-        file_extension = os.path.splitext(file_path)[1].lower()
-        if file_extension in ['.csv', '.txt']:
+        details['extension'] = os.path.splitext(file_path)[1].lower()
+        if details['extension'] in ['.csv', '.txt']:
             with open(file_path, 'rb') as f:
                 content_bytes = f.read()
             detected = chardet.detect(content_bytes)
@@ -63,7 +64,7 @@ def get_details_file(file_path, header=None):
                 details['rows'] = sum(1 for row in f)
             if header is not None:
                 details['check_header'] = get_header_check(details['header'], details['delimiter'], header)
-        elif file_extension in ['.xls', '.xlsx']:
+        elif details['extension'] in ['.xls', '.xlsx']:
             with openpyxl.load_workbook(file_path, read_only=True) as wb:
                 details['encoding'] = wb.encoding
                 details['confidence'] = 1.0
@@ -116,7 +117,7 @@ def get_data_file(file_path):
     return data_list_dict
 
 
-def set_data_csv(file_path, data_list_dict, to_utf8=False):
+def set_data_csv(file_path, data_list_dict={}, to_utf8=False):
     if to_utf8:
         if os.path.isfile(file_path):
             details = get_details_file(file_path)
@@ -159,6 +160,16 @@ def get_file_path(path, filename):
     return result
 
 
+def set_log():
+    logger = logging.getLogger('et-csv')
+    ch = logging.StreamHandler()
+    ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(ch)
+    logger.setLevel(logging.DEBUG)
+
+    return logger
+
+
 def copy_file(file_path, path):
     if os.path.isfile(file_path):
         file_path_copy = os.path.join(path, os.path.basename(file_path))
@@ -180,12 +191,12 @@ def usage():
     them into csv with a common utf-8 format.
     
     Usage:
-      python main.py --help
-      python main.py --version
-      python main.py -i <infile> -f <field> -c <copy>
-      python main.py -i <infile> -f <field> -c <copy> -s <search>
-      python main.py -i <infile> -f <field> -c <copy> -H <header>
-      python main.py -i <infile> -f <field> -c <copy> -s <search> -H <header>
+      python et-csv.py --help
+      python et-csv.py --version
+      python et-csv.py -i <infile> -f <field> -c <copy>
+      python et-csv.py -i <infile> -f <field> -c <copy> -s <search>
+      python et-csv.py -i <infile> -f <field> -c <copy> -H <header>
+      python et-csv.py -i <infile> -f <field> -c <copy> -s <search> -H <header>
     
     Mandatory arguments to long options are mandatory for short options too.
       -i, --input=STRING file path with the list of files
@@ -237,114 +248,50 @@ def parse():
     if infile is None or field is None or copy is None:
         usage()
         sys.exit()
-    return infile, field, copy, search, header
+    return {'infile': infile, 'field': field, 'copy': copy, 'search': search, 'header': header}
 
 
 def main():
-    print(parse())
-
-#    file_list_files = None
-#    field_name_file = None
-#    path_to_copy = None
-#    path_to_search = None
-#    header_valid = None
-#    try:
-#        opts, args = getopt.getopt(argv, 'i:c:p:s:v:')
-#    except getopt.GetoptError:
-#        print('main.py'
-#              ' -i <file_list_files>'
-#              ' -c <field_name_file>'
-#              ' -p <path_to_copy>'
-#              ' -s <path_to_search>'
-#              ' -H <header_valid>')
-#        sys.exit(2)
-#    for opt, arg in opts:
-#        if opt == '-h':
-#            print('''
-#                  main.py
-#                  -i file with list of necessary files
-#                  -c field of file with name of necessary files
-#                  -p path to save necessary files
-#                  -s path to search files
-#                  -v (optional) header valid on necessary files
-#                  ''')
-#            sys.exit()
-#        elif opt in ('-i', '--file_list_files'):
-#            file_list_files = arg
-#        elif opt in ('-c', '--field_name_file'):
-#            field_name_file = arg
-#        elif opt in ('-p', '--path_to_copy'):
-#            path_to_copy = arg
-#        elif opt in ('-s', '--path_to_search'):
-#            path_to_search = arg
-#        elif opt in ('-v', '--header_valid'):
-#            header_valid = arg
-
-#    print('Input file is', file_list_files)
-#    print('Column file is', field_name_file)
-#    print('Path to files is', path_to_copy)
-#    print('Path to search is', path_to_search)
-#    print('Header valid is', header_valid)
-#    path_to_search = r'C:\Users\fpx1vg\Desktop\BBDD'
-#    path_to_copy = r'C:\Users\fpx1vg\Desktop\BBDD 2020'
-#    csv_list_files = r'C:\Users\fpx1vg\Desktop\CARGAS_2020.csv'
-#    csv_list_result = r'C:\Users\fpx1vg\Desktop\CARGAS_2020_RESULTADO.csv'
-#    field_name_file = 'CARGA'
-#    header_valid = 'LEAD_ID;E_CUPS;E_FIGURA_JUR;E_TITULAR_NOMBRE;E_TITULAR_APELLIDO1;E_TITULAR_APELLIDO2;E_TELEFONO;
-#    E_TELEFONO2;E_TELEFONO3;E_TELEFONO4;E_EMAIL1;E_EMAIL2;E_MULTI;E_TANDA;E_PRIORIDAD;E_TARIFA;E_TENSION;
-#    E_POTENCIA_CONTRATADA_P1;E_POTENCIA_CONTRATADA_P2;E_POTENCIA_CONTRATADA_P3;E_POTENCIA_NORMALIZADA;
-#    E_PS_PROVINCIA;E_PS_MUNICIPIO;E_PS_POBLACION;E_PS_CODIGO_POSTAL;E_PS_TIPO_VIA;E_PS_CALLE;E_PS_NUMERO;E_PS_EDIFICIO;
-#    E_PS_ESCALERA;E_PS_PORTAL;E_PS_PISO;E_PS_PUERTA;E_PS_ACLARADOR;E_DOC_NOMBRE;E_DOC_APELLIDO1;E_DOC_APELLIDO2;
-#    E_DOC_PROVINCIA;E_DOC_MUNICIPIO;E_DOC_POBLACION;E_DOC_CODIGO_POSTAL;E_DOC_TIPO_VIA;E_DOC_CALLE;E_DOC_NUMERO;
-#    E_DOC_EDIFICIO;E_DOC_PORTAL;E_DOC_ESCALERA;E_DOC_PISO;E_DOC_PUERTA;E_DOC_ACLARADOR;E_CLI_NOMBRE;E_CLI_APELLIDO1;
-#    E_CLI_APELLIDO2;E_CLI_PROVINCIA;E_CLI_MUNICIPIO;E_CLI_POBLACION;E_CLI_CODIGO_POSTAL;E_CLI_TIPO_VIA;E_CLI_CALLE;
-#    E_CLI_NUMERO;E_CLI_EDIFICIO;E_CLI_PORTAL;E_CLI_ESCALERA;E_CLI_PISO;E_CLI_PUERTA;E_CLI_ACLARADOR;G_CUPS;
-#    G_FIGURA_JUR;G_TITULAR_NOMBRE;G_TITULAR_APELLIDO1;G_TITULAR_APELLIDO2;G_TELEFONO;G_TELEFONO2;G_TELEFONO3;
-#    G_TELEFONO4;G_EMAIL1;G_EMAIL2;G_MULTI;G_TANDA;G_PRIORIDAD;G_TARIFA;G_PS_PROVINCIA;G_PS_MUNICIPIO;G_PS_POBLACION;
-#    G_PS_CODIGO_POSTAL;G_PS_TIPO_VIA;G_PS_CALLE;G_PS_NUMERO;G_PS_EDIFICIO;G_PS_ESCALERA;G_PS_PORTAL;G_PS_PISO;
-#    G_PS_PUERTA;G_PS_ACLARADOR;G_DOC_NOMBRE;G_DOC_APELLIDO1;G_DOC_APELLIDO2;G_DOC_PROVINCIA;G_DOC_MUNICIPIO;
-#    G_DOC_POBLACION;G_DOC_CODIGO_POSTAL;G_DOC_TIPO_VIA;G_DOC_CALLE;G_DOC_NUMERO;G_DOC_EDIFICIO;G_DOC_PORTAL;
-#    G_DOC_ESCALERA;G_DOC_PISO;G_DOC_PUERTA;G_DOC_ACLARADOR;G_CLI_NOMBRE;G_CLI_APELLIDO1;G_CLI_APELLIDO2;
-#    G_CLI_PROVINCIA;G_CLI_MUNICIPIO;G_CLI_POBLACION;G_CLI_CODIGO_POSTAL;G_CLI_TIPO_VIA;G_CLI_CALLE;G_CLI_NUMERO;
-#    G_CLI_EDIFICIO;G_CLI_PORTAL;G_CLI_ESCALERA;G_CLI_PISO;G_CLI_PUERTA;G_CLI_ACLARADOR;E_FECHA_CAMBIO_COMERC;
-#    E_CONSUMO_ULT_12M;G_FECHA_CAMBIO_COMERC;G_CONSUMO_ULT_12M;ORIGEN;FRANJA_HORARIA_RECOMENDADA;E_PRODUCTO_RECOMENDADO;
-#    G_PRODUCTO_RECOMENDADO;ANTIG_CONTRATO_ACTUAL;E_ZONA_GASIFICADA;SONDEO;RACIMO;CONSENTIMIENTO;E_COMERCIALIZADORA;
-#    E_COMERCIALIZADORA_CUR;G_COMERCIALIZADORA;G_COMERCIALIZADORA_CUR;AUX_01;AUX_02;AUX_03;AUX_04;AUX_05;AUX_06;AUX_07;
-#    AUX_08;AUX_09;AUX_10;AUX_11;AUX_12;AUX_13;AUX_14;AUX_15;AUX_16;AUX_17;AUX_18;AUX_19;AUX_20;AUX_21;AUX_22;AUX_23;
-#    AUX_24;AUX_25;AUX_26;AUX_27;AUX_28;AUX_29;AUX_30;AUX_31;AUX_32;AUX_33;AUX_34;AUX_35;AUX_36;AUX_37;AUX_38;AUX_39;
-#    AUX_40;AUX_41;AUX_42;AUX_43;AUX_44;AUX_45;AUX_46;AUX_47;AUX_48;AUX_49;AUX_50'
-
-#    csv_list_dict = read_csv(csv_list_files)
-#    counter = 1
-#    for row in csv_list_dict:
-#        try:
-#            if field_name_file in row.keys():
-#                print(f'Procesando el fichero {counter}: {row[field_name_file]} ...')
-#                counter = counter + 1
-#                path_file_search = search_file(path_to_search, row[field_name_file])
-#                path_file_dst = os.path.join(path_to_copy, row[field_name_file])
-#                if os.path.isfile(path_file_dst):
-#                    row.update({'log': 'ficheiro existente'})
-#                    row.update(data_file_details(path_file_dst))
-#                    row.update({'header': check_header(row['header'], header_valid)})
-#                elif os.path.isfile(path_file_search):
-#                    copy_file(path_file_search, path_to_copy)
-#                    row.update({'log': 'ficheiro copiado'})
-#                    row.update(data_file_details(path_file_dst))
-#                    row.update({'header': check_header(row['header'], header_valid)})
-#                else:
-#                    row.update({'log': 'fichero non existe'})
-#                    row.update(data_file_details(''))
-#            else:
-#                raise Exception(f'Non existe o campo {field_name_file} no listado de ficheiros')
-
-
-#        except Exception as e:
-#            row.update({'log': f'ficheiro provoca un erro {e.__str__()}'})
-#            row.update(data_file_details(''))
-#            pass
-
-# write_csv(csv_list_result, csv_list_dict)
+    log = set_log()
+    params = parse()
+    log.info('Start script')
+    infile = params['infile']
+    field = params['field']
+    copy = params['copy']
+    search = params['search']
+    header = params['header']
+    log.info(f'Read input file: {infile}')
+    in_list_dict = get_data_file(infile)
+    if len(in_list_dict) == 0:
+        log.warning(f'No data on input file {infile}')
+    counter = 1
+    for row in in_list_dict:
+        if field in row.keys():
+            log.info(f'Searching file {counter}/{len(in_list_dict)} {row[field]}')
+            file_copy = get_file_path(copy, row[field])
+            if search is not None:
+                file_search = get_file_path(search, row[field])
+            if os.path.isfile(file_copy):
+                log.info(f'File already exists')
+                row.update({'log': 'file exists'})
+            elif os.path.isfile(file_search):
+                copy_file(file_search, copy)
+                log.info(f'File copied')
+                row.update({'log': 'file copied'})
+            else:
+                log.info(f'File not found')
+                row.update({'log': 'file not found'})
+            row.update(get_details_file(file_copy, header))
+        else:
+            log.warning(f'Not found field {field} on in file {infile}')
+            break
+        if row['extension'] in ['.csv', '.txt'] and row['encoding'] not in ['utf-8', 'ascii']:
+            log.info(f'Convert encode file from {row["encoding"]} to utf-8')
+            set_data_csv(file_copy, to_utf8=True)
+            row.update(get_details_file(file_copy, header))
+            log.info(f'Converted to {row["encoding"]}')
+        counter = counter + 1
+    set_data_csv(f'{os.path.splitext(infile)[0]}_result.csv', in_list_dict)
 
 
 if __name__ == '__main__':
